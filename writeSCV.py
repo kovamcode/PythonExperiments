9cd313e2ffe03cd823ecd763be9c781418b80c11
kovamcode <maksymkovalenko@icloud.com>
2023-11-09 23:12:03 -0700

    create new CSV file by script

--- writeSCV.py
+++ writeSCV.py
@@ -2,3 +2,14 @@ olympians = [("John Aalberg", 31, "Cross Country Skiing"),
              ("Minna Maarit Aalto", 30, "Sailing"),
              ("Win Valdemar Aaltonen", 54, "Art Competitions"),
              ("Wakako Abe", 18, "Cycling")]
+
+outfile = open('writed_olympics.csv', "w")
+
+outfile.write('Name,Age,Sport')
+outfile.write('\n')
+
+for olympian in olympians:
+    row_string = '{},{},{}'. format(olympian[0], olympian[1], olympian[2])
+    outfile.write(row_string)
+    outfile.write('\n')
+outfile.close
