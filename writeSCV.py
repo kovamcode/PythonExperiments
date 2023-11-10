2eba29ad6946c80cb3865bc6468d68af81597ab0
kovamcode <maksymkovalenko@icloud.com>
2023-11-09 23:43:14 -0700

    fix bug with comas on string, by using .format and bracers

--- writeSCV.py
+++ writeSCV.py
@@ -1,18 +1,18 @@
-olympians = [("John Aalberg", 31, "Cross Country Skiing"),
+olympians = [("John, Aalberg", 31, "Cross Country Skiing"),
              ("Minna Maarit Aalto", 30, "Sailing"),
              ("Win Valdemar Aaltonen", 54, "Art Competitions"),
              ("Wakako Abe", 18, "Cycling")]
 
 outfile = open('writed_olympics.csv', "w")
-
-outfile.write('Name,Age,Sport')
+outfile.write('"Name","Age","Sport"')
 outfile.write('\n')
 
 for olympian in olympians:
-      #.format use when we have integers and strings, .join method when we have only strings,
-    row_string = ','.join([olympian[0], str(olympian[1]), olympian[2]])
+    #.format use when we have integers and strings, .join method when we have only strings,
+    #row_string = ",".join(olympian) 
+    #row_string = ','.join([olympian[0], str(olympian[1]), olympian[2]])
   
-    #row_string = '{},{},{}'. format(olympian[0], olympian[1], olympian[2])
+    row_string = '"{}","{}","{}"'. format(olympian[0], olympian[1], olympian[2])
     outfile.write(row_string)
     outfile.write('\n')
 outfile.close
